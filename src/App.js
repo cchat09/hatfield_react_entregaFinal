d96eb1a8f793d97bce9c6ecd21b91d789f455240
Christopher Hatfield <ccullenhatfield@gmail.com>
2024-02-21 21:27:20 +0100

    basic navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyNavbar from './components/navigation/navbar.js';
 
 function App() {
   return (
     <div className="App">
+    <MyNavbar></MyNavbar>
+      <h1>hello</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
